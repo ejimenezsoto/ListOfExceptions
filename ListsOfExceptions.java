67b8bc6e441d9329089fb418ed149ea84af14893
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-02-16 14:00:40 -0800

    Project Complete

---
+++ ListsOfExceptions.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class ListsOfExceptions{
+    public static void main(String[] args){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world"); 
+        myList.add(48);
+        myList.add("Goodbye World");
+
+        for(int i =0;i < myList.size(); i++){
+            try{
+                Integer castedValue = (Integer) myList.get(i);
+            }catch(ClassCastException e){
+                System.out.println("Error: " + e.getMessage() + " Index: " + i + " Value: " + myList.get(i));
+            }
+            
+        }
+    }
+    
+
+}
